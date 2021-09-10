2f70ce60f30cfa1eef1c6ea4539e11590e23c634
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-09-09 21:41:44 -0300

    Recuperando dados na single de continente

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,11 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, Box } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { Container } from '../Container'
 
 export function Navbar() {
+  const { pathname } = useRouter()
+  
   return (
     <Flex
       h="100px"
@@ -11,7 +15,23 @@ export function Navbar() {
       justifyContent="center"
       position="relative"
     >
-      <Image src='/Logo.svg' />
+      <Container>
+        <Box
+          position="absolute"
+          top="50%"
+          left="0"
+          transform="translateY(-50%)"
+        >
+          {pathname !== "/" && (
+            <Link href="/">
+              <a>
+                <Image src='/back.svg' />
+              </a>
+            </Link>
+          )}
+        </Box>
+        <Image src='/Logo.svg' margin="0 auto"/>
+      </Container>
     </Flex>
   )
 }
--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,12 +1,67 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, Box, Text, SimpleGrid } from "@chakra-ui/react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Container } from "../../components/Container";
 import { Navbar } from "../../components/Navbar";
+import api from "../../services/api";
 
-export default function Continent() {
+interface ContinentProps {
+  continent: {
+    slug: string
+    title: string
+    content: string
+    language: string
+    contries: string
+    cidades: string
+  }
+}
+
+export default function Continent({ continent }: ContinentProps) {
+  console.log(continent)
   return (
     <Flex
       flexDir="column"
     >
       <Navbar />
+      <Flex 
+        h="500px" 
+        background="url('/cover.png')"
+        backgroundSize="cover"
+        backgroundPosition="center"
+      >
+        <Container>
+          <Box h="100%" display="flex" alignItems="flex-end">
+            <Heading color="#fff" marginBottom="50px">{continent.title}</Heading>
+          </Box>
+        </Container>
+      </Flex>
+      <Container>
+        <SimpleGrid  templateColumns="1fr 1fr" gridGap="70px">
+          <Text color="#47585B" fontSize="24px" lineHeight="36px" textAlign="justify">{continent.content}</Text>
+        </SimpleGrid>
+      </Container>
     </Flex>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('/continent') 
+
+  const paths = data.map((continent) => ({
+    params: { slug: continent.slug },
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await api.get(`/continent?slug=${String(context.params.slug)}`)
+  const continent = data[0]
+  return {
+    props: {
+      continent
+    }
+  }
 }
